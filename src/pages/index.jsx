123f667e45815a28d9698d50029aa4bacea22628
Jorge Barron <jorgeb8168711@gmail.com>
2020-04-21 19:23:01 -0500

    Updated title

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Create Next App</title>
+        <title>jbarron</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
